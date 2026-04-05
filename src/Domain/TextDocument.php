685823459894265aa04af388d791ef86b5328b50
eugene <zhenyavernigora@gmail.com>
2026-04-05 19:16:16 +0300

    add validation

--- src/Domain/TextDocument.php
+++ src/Domain/TextDocument.php
@@ -2,12 +2,17 @@
 
 namespace Domain;
 
+use InvalidArgumentException;
+
 class TextDocument
 {
     private string $content;
 
     public function __construct(string $content)
     {
+        if (trim($content) === '') {
+            throw new InvalidArgumentException("Text cannot be empty");
+        }
         $this->content = $content;
     }
 
