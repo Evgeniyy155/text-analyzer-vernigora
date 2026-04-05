fb58596b9e1d0215c271f54b2a54cf7afee6572b
eugene <zhenyavernigora@gmail.com>
2026-04-05 19:15:42 +0300

    add input handling

---
+++ src/Application/AnalyzerService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Application;
+
+use Domain\TextDocument;
+
+class AnalyzerService
+{
+    public function analyze(string $text): array
+    {
+        $document = new TextDocument($text);
+        return [
+            'words' => $document->getWordCount()
+        ];
+    }
+}
